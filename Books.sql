e3aca0777c993ec1a050013d9db3d351d6e3c540
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-11-26 19:30:18 +0530

    books

---
+++ Books.sql
@@ -0,0 +1,18 @@
+USE [Day1Task]
+GO
+
+/****** Object:  Table [dbo].[Books]    Script Date: 26-11-2025 16:05:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Books](
+	[Title] [varchar](100) NULL,
+	[Auther] [varchar](50) NULL,
+	[PublicationYear] [int] NULL
+) ON [PRIMARY]
+GO
+
+
